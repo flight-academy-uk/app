00487c4ac487a97f002a261eb4de741b16c0963c
Thunder <robert.shalders@hotmail.com>
2024-11-10 14:44:00 +0000

    Updated SST configuration to use correct AWS profile

--- sst.config.ts
+++ sst.config.ts
@@ -1,23 +1,30 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
+import { readdirSync } from "fs";
+
 export default $config({
   app(input) {
     return {
       name: "flight-academy",
       providers: {
         aws: {
-          profile: input.stage === "production" ? "flight-academy-production" : "flight-academy-dev"
-        }
+          profile:
+            input.stage === "production"
+              ? "flight-academy-production"
+              : "flight-academy-dev",
+          region: "eu-west-2",
+        },
       },
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
     };
   },
   async run() {
-    const api = await import("./infra/api");
-
-    return {
-      api: api.myApi.url,
-    };
+    const outputs = {};
+    for (const resource of readdirSync("./infra/")) {
+      const result = await import("./infra/" + resource);
+      if (result.outputs) Object.assign(outputs, result.outputs);
+    }
+    return outputs;
   },
 });
