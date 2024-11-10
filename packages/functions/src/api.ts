15c11c0ce5ba00322927e65934c0afc95d72ce48
Thunder <robert.shalders@hotmail.com>
2024-11-10 01:23:14 +0000

    Removed examples

--- packages/functions/src/api.ts
+++
@@ -1,9 +0,0 @@
-import { Handler } from "aws-lambda";
-import { Example } from "@flight-academy/core/example";
-
-export const handler: Handler = async (_event) => {
-  return {
-    statusCode: 200,
-    body: `"${Example.hello()}"`,
-  };
-};
