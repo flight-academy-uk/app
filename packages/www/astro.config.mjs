a640286c2663e3b9cafcbe5e72eae858aa864a2a
Thunder <robert.shalders@hotmail.com>
2024-11-24 22:52:27 +0000

    Removed "default" tailwind base inclusion in each file, and moved into a global CSS. Further, various Google fonts have been added

--- packages/www/astro.config.mjs
+++ packages/www/astro.config.mjs
@@ -7,7 +7,12 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  security: {
+    checkOrigin: true
+  },
   adapter: aws(),
-  output: 'server',
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), svelte()],
 });
